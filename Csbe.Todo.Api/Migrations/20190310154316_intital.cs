6b841c5f24b288caaae2f976c6a84a2e1ef6952b
vometablissement <hoechstesetablissement@gmail.com>
2019-03-10 17:13:20 +0100

    added properties in the TodoItem model

---
+++ Csbe.Todo.Api/Migrations/20190310154316_intital.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Csbe.Todo.Api.Migrations
+{
+    public partial class intital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PwSalt",
+                table: "Users");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FinishDate",
+                table: "TodoItems",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Importance",
+                table: "TodoItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FinishDate",
+                table: "TodoItems");
+
+            migrationBuilder.DropColumn(
+                name: "Importance",
+                table: "TodoItems");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PwSalt",
+                table: "Users",
+                nullable: true);
+        }
+    }
+}
--- Csbe.Todo.Api/Models/TodoItem.cs
+++ Csbe.Todo.Api/Models/TodoItem.cs
@@ -8,6 +8,10 @@ namespace Csbe.Todo.Api.Models
 
         public string Name {get; set;}
 
+        public DateTime FinishDate {get;set;}
+
+        public string Importance {get;set;}
+
         public bool IsComplete {get; set;}
 
         public User User {get;set;}
