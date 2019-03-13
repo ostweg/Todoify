50bd06da5a82be62b7ebdb254290aadb4e0147f8
vometablissement <hoechstesetablissement@gmail.com>
2019-03-13 22:10:59 +0100

    changed models

---
+++ Csbe.Todo.Api/Migrations/20190313183222_init13.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Csbe.Todo.Api.Migrations
+{
+    public partial class init13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedById",
+                table: "TodoItems",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "TodoItems");
+        }
+    }
+}
---
+++ Csbe.Todo.Api/Migrations/20190313192743_init14.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Csbe.Todo.Api.Migrations
+{
+    public partial class init14 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "TodoItems");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "TodoItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "TodoItems");
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedById",
+                table: "TodoItems",
+                nullable: false,
+                defaultValue: 0L);
+        }
+    }
+}
